332704635ccec9c2027a5af545a91e35dc576991
Kyle Bachinski <kylegb2@illinois.edu>
2019-06-09 14:17:59 -0500

    adding server.rs

---
+++ src/server.rs
@@ -0,0 +1,19 @@
+use std::net::*;
+use std::thread;
+
+fn main() {
+    let mut server: TcpListener = TcpListener::bind("127.0.0.1:6000").unwrap();
+
+    for client in server.incoming() {
+        match client {
+            Ok(client) => { 
+                thread::spawn(move || { handle_client(client); }); 
+            }
+            Err(err) => { eprintln!("error: {:?}", err); }
+        }
+    }
+}
+
+fn handle_client(client: TcpStream) {
+
+}
